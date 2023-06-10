0c4e0cc8da8d726c08d98faa113e2abef6585dd5
AmanJadon2023 <aman.jadon@geminisolutions.com>
2023-06-10 13:21:11 +0530

    clean up functions learning

--- src/pages/LandingPage/index.js
+++ src/pages/LandingPage/index.js
@@ -1,10 +1,33 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
 const LandingPage = () => {
+  // const [state, setState] = useState();
+  // const timeout = () => {
+  //   return new Promise(function (myResolve, myReject) {
+  //     setTimeout(() => {
+  //       myResolve(); // when successful
+  //     }, 1000);
+  //     // when error
+  //   });
+  // };
+  // useEffect(() => {
+  //   let isCancel = false;
+  //   async function handleChange() {
+  //     await timeout();
+  //     if (!isCancel)
+  //       alert('hi your name is ' + state);
+  //   }
+  //   handleChange();
+  //   console.log('object');
+  //   return () => {
+  //     isCancel = true;
+  //   };
+  // }, [state]);
   return (
     <div>
-        Landing page
+      {/* <input className='border-2  p-2 ml-96 mt-48 border-black' placeholder='enter' value={state} onChange={(e) => { setState(e.target.value); }} /> */}
+      Landing page
     </div>
-  )
-}
+  );
+};
 
-export default LandingPage
+export default LandingPage;
